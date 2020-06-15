3e5475e012e46fe7db9403cd8f87aef6476c7549
Gerald Venzl <gerald.venzl@gmail.com>
2020-06-14 19:05:56 -0700

    Add more tests, refactor
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- test/python/loading_tests.py
+++ test/python/loading_tests.py
@@ -25,12 +25,13 @@ import config as cfg
 import unittest
 import csv2db
 
-login = {
+test_parameters = {
     "user": "test",
     "db2_user": "db2inst1",
     "password": "LetsDocker1",
     "database": "test",
-    "table": "STAGING"
+    "table_staging": "STAGING",
+    "table_locations": "LOCATIONS"
 }
 
 
@@ -44,91 +45,16 @@ class LoadingTestCaseSuite(unittest.TestCase):
         cfg.debug = False
         cfg.truncate_before_load = False
 
-    def test_loading_MySQL(self):
-        print("test_loading_MySQL")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/201811-citibike-tripdata.csv",
-                              "-o", "mysql",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", login["table"]
-                              ]
-                            )
-                         )
-
-    def test_loading_Postgres(self):
-        print("test_loading_Postgres")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/201811-citibike-tripdata.csv",
-                              "-o", "postgres",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", login["table"]
-                              ]
-                            )
-                         )
-
-    def test_loading_Oracle(self):
-        print("test_loading_Oracle")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/201811-citibike-tripdata.csv",
-                              "-o", "oracle",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", login["table"]
-                              ]
-                            )
-                         )
-
-    def test_loading_SqlServer(self):
-        print("test_loading_SqlServer")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                              ["load",
-                               "-f", "../resources/201811-citibike-tripdata.csv",
-                               "-o", "sqlserver",
-                               "-u", login["user"],
-                               "-p", login["password"],
-                               "-d", login["database"],
-                               "-t", login["table"]
-                               ]
-                             )
-                         )
-
-    def test_loading_Db2(self):
-        print("test_loading_Db2")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                              ["load",
-                               "-f", "../resources/201811-citibike-tripdata.csv",
-                               "-o", "db2",
-                               "-u", login["db2_user"],
-                               "-p", login["password"],
-                               "-d", login["database"],
-                               "-t", login["table"]
-                               ]
-                             )
-                         )
-
     def test_load_file_with_insufficient_columns(self):
         print("test_load_file_with_insufficient_columns")
         self.assertEqual(f.ExitCodes.SUCCESS.value,
                          csv2db.run(
                               ["load",
                                "-f", "../resources/bad/201811-citibike-tripdata-not-enough-columns.csv",
-                               "-u", login["user"],
-                               "-p", login["password"],
-                               "-d", login["database"],
-                               "-t", login["table"],
+                               "-u", test_parameters["user"],
+                               "-p", test_parameters["password"],
+                               "-d", test_parameters["database"],
+                               "-t", test_parameters["table_staging"],
                                "--debug"
                                ]
                              )
@@ -152,9 +78,9 @@ class LoadingTestCaseSuite(unittest.TestCase):
                          csv2db.run(
                               ["load",
                                "-f", "../resources/201811-citibike-tripdata.csv",
-                               "-u", login["user"],
-                               "-p", login["password"],
-                               "-d", login["database"],
+                               "-u", test_parameters["user"],
+                               "-p", test_parameters["password"],
+                               "-d", test_parameters["database"],
                                "-t", "DOES_NOT_EXIST",
                                "--debug"
                                ]
@@ -167,149 +93,198 @@ class LoadingTestCaseSuite(unittest.TestCase):
                          csv2db.run(
                              ["load",
                               "-f", "../resources/bad/201811-citibike-tripdata-empty.csv",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", login["table"]
+                              "-u", test_parameters["user"],
+                              "-p", test_parameters["password"],
+                              "-d", test_parameters["database"],
+                              "-t", test_parameters["table_staging"]
                               ]
                             )
                          )
 
+    def test_loading_MySQL(self):
+        print("test_loading_MySQL")
+        helper_load_data(self, f.DBType.MYSQL.value, "../resources/201811-citibike-tripdata.csv")
+
+    def test_loading_Postgres(self):
+        print("test_loading_Postgres")
+        helper_load_data(self, f.DBType.POSTGRES.value, "../resources/201811-citibike-tripdata.csv")
+
+    def test_loading_Oracle(self):
+        print("test_loading_Oracle")
+        helper_load_data(self, f.DBType.ORACLE.value, "../resources/201811-citibike-tripdata.csv")
+
+    def test_loading_SQLServer(self):
+        print("test_loading_SQLServer")
+        helper_load_data(self, f.DBType.SQLSERVER.value, "../resources/201811-citibike-tripdata.csv")
+
+    def test_loading_Db2(self):
+        print("test_loading_Db2")
+        helper_load_data(self,
+                         f.DBType.DB2.value,
+                         "../resources/201811-citibike-tripdata.csv",
+                         username=test_parameters["db2_user"])
+
     def test_unicode_file_Oracle(self):
         print("test_unicode_file_Oracle")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/allCountries.1000.txt.gz",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", "LOCATIONS",
-                              "-s", "	"]
-                             )
-                         )
+        helper_load_data(self,
+                         f.DBType.ORACLE.value,
+                         "../resources/allCountries.1000.txt.gz",
+                         table=test_parameters["table_locations"],
+                         separator="\t")
 
     def test_unicode_file_MySQL(self):
         print("test_unicode_file_MySQL")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/allCountries.1000.txt.gz",
-                              "-o", "mysql",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", "LOCATIONS",
-                              "-s", "	"]
-                             )
-                         )
+        helper_load_data(self,
+                         f.DBType.MYSQL.value,
+                         "../resources/allCountries.1000.txt.gz",
+                         table=test_parameters["table_locations"],
+                         separator="\t")
 
     def test_unicode_file_Postgres(self):
         print("test_unicode_file_Postgres")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/allCountries.1000.txt.gz",
-                              "-o", "postgres",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", "LOCATIONS",
-                              "-s", "	"]
-                             )
-                         )
+        helper_load_data(self,
+                         f.DBType.POSTGRES.value,
+                         "../resources/allCountries.1000.txt.gz",
+                         table=test_parameters["table_locations"],
+                         separator="\t")
 
-    def test_unicode_file_SqlServer(self):
-        print("test_unicode_file_SqlServer")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/allCountries.1000.txt.gz",
-                              "-o", "sqlserver",
-                              "-u", login["user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", "LOCATIONS",
-                              "-s", "	"]
-                             )
-                         )
+    def test_unicode_file_SQLServer(self):
+        print("test_unicode_file_SQLServer")
+        helper_load_data(self,
+                         f.DBType.SQLSERVER.value,
+                         "../resources/allCountries.1000.txt.gz",
+                         table=test_parameters["table_locations"],
+                         separator="\t")
 
     def test_unicode_file_Db2(self):
         print("test_unicode_file_Db2")
-        self.assertEqual(f.ExitCodes.SUCCESS.value,
-                         csv2db.run(
-                             ["load",
-                              "-f", "../resources/allCountries.1000.txt.gz",
-                              "-o", "db2",
-                              "-u", login["db2_user"],
-                              "-p", login["password"],
-                              "-d", login["database"],
-                              "-t", "LOCATIONS",
-                              "-s", "	"]
-                             )
-                         )
+        helper_load_data(self,
+                         f.DBType.DB2.value,
+                         "../resources/allCountries.1000.txt.gz",
+                         table=test_parameters["table_locations"],
+                         separator="\t",
+                         username=test_parameters["db2_user"])
 
     def test_truncate_table_before_load_Oracle(self):
         print("test_truncate_table_before_load_Oracle")
-        truncate_table_before_load(self, f.DBType.ORACLE.value)
+        helper_truncate_table_before_load(self, f.DBType.ORACLE.value)
 
     def test_truncate_table_before_load_MySQL(self):
         print("test_truncate_table_before_load_MySQL")
-        truncate_table_before_load(self, f.DBType.MYSQL.value)
+        helper_truncate_table_before_load(self, f.DBType.MYSQL.value)
 
     def test_truncate_table_before_load_Postgres(self):
         print("test_truncate_table_before_load_Postgres")
-        truncate_table_before_load(self, f.DBType.POSTGRES.value)
+        helper_truncate_table_before_load(self, f.DBType.POSTGRES.value)
 
-    def test_truncate_table_before_load_SqlServer(self):
+    def test_truncate_table_before_load_SQLServer(self):
         print("test_truncate_table_before_load_SQLServer")
-        truncate_table_before_load(self, f.DBType.SQLSERVER.value)
+        helper_truncate_table_before_load(self, f.DBType.SQLSERVER.value)
 
     def test_truncate_table_before_load_Db2(self):
         print("test_truncate_table_before_load_Db2")
-        truncate_table_before_load(self, f.DBType.DB2.value, login["db2_user"])
+        helper_truncate_table_before_load(self, f.DBType.DB2.value, test_parameters["db2_user"])
+
+    def test_negative_truncate_table_before_load_Oracle(self):
+        print("test_negative_truncate_table_before_load_Oracle")
+        helper_negative_truncate_table_before_load(self, f.DBType.ORACLE.value)
+
+    def test_negative_truncate_table_before_load_MySQL(self):
+        print("test_negative_truncate_table_before_load_MySQL")
+        helper_negative_truncate_table_before_load(self, f.DBType.MYSQL.value)
+
+    def test_negative_truncate_table_before_load_Postgres(self):
+        print("test_negative_truncate_table_before_load_Postgres")
+        helper_negative_truncate_table_before_load(self, f.DBType.POSTGRES.value)
+
+    def test_negative_truncate_table_before_load_SQLServer(self):
+        print("test_negative_truncate_table_before_load_SQLServer")
+        helper_negative_truncate_table_before_load(self, f.DBType.SQLSERVER.value)
+
+    def test_negative_truncate_table_before_load_Db2(self):
+        print("test_negative_truncate_table_before_load_Db2")
+        helper_negative_truncate_table_before_load(self, f.DBType.DB2.value, test_parameters["db2_user"])
 
 
-def truncate_table_before_load(self, db_type, username=login["user"]):
+def helper_negative_truncate_table_before_load(self, db_type, username=test_parameters["user"]):
+    conn = helper_get_db_conn(db_type, username)
+    f.truncate_table(db_type, conn, test_parameters["table_staging"])
+    conn.close()
+    count1 = helper_load_data(self, db_type,
+                              file="../resources/201811-citibike-tripdata.csv",
+                              table=test_parameters["table_staging"],
+                              username=username)
+    count2 = helper_load_data(self, db_type,
+                              file="../resources/201811-citibike-tripdata.csv",
+                              table=test_parameters["table_staging"],
+                              username=username)
+    # Assert that double the amount of rows have been loaded (2 * first count == second count)
+    self.assertEqual((count1*2), count2)
 
+
+def helper_load_data(self, db_type, file,
+                     table=test_parameters["table_staging"],
+                     username=test_parameters["user"],
+                     separator=","):
+    self.assertEqual(f.ExitCodes.SUCCESS.value,
+                     csv2db.run(
+                         ["load",
+                          "-f", file,
+                          "-o", db_type,
+                          "-u", username,
+                          "-p", test_parameters["password"],
+                          "-d", test_parameters["database"],
+                          "-t", table,
+                          "-s", separator
+                          ]
+                        )
+                     )
+    return helper_get_count_from_table(db_type, username, table)
+
+
+def helper_truncate_table_before_load(self, db_type, username=test_parameters["user"]):
     params = ["load",
               "-f", "../resources/201811-citibike-tripdata.csv*",
               "-o", db_type,
               "-u", username,
-              "-p", login["password"],
-              "-d", login["database"],
-              "-t", login["table"],
+              "-p", test_parameters["password"],
+              "-d", test_parameters["database"],
+              "-t", test_parameters["table_staging"],
               "--truncate"
               ]
 
     self.assertEqual(f.ExitCodes.SUCCESS.value, csv2db.run(params))
-    # MySQL: a connection cannot hold the talbe still in cache
+    # MySQL: a connection cannot hold the table still in cache
     # otherwise the TRUNCATE TABLE will hang (as it is a DROP/CREATE TABLE)
-    count1 = get_count_from_table(db_type, username, login["table"])
+    count1 = helper_get_count_from_table(db_type, username, test_parameters["table_staging"])
 
     # Reset global truncate parameter
     cfg.truncate_before_load = False
 
     self.assertEqual(f.ExitCodes.SUCCESS.value, csv2db.run(params))
-    count2 = get_count_from_table(db_type, username, login["table"])
+    count2 = helper_get_count_from_table(db_type, username, test_parameters["table_staging"])
 
     self.assertEqual(count1, count2)
 
 
-def get_count_from_table(db_type, username, table):
-    conn = f.get_db_connection(db_type,
-                               username,
-                               login["password"],
-                               "localhost",
-                               f.get_default_db_port(db_type),
-                               login["database"])
+def helper_get_count_from_table(db_type, username, table):
+    conn = helper_get_db_conn(db_type, username)
     cur = conn.cursor()
-    cur.execute("SELECT COUNT(1) FROM {0}".format(login["table"]))
+    cur.execute("SELECT COUNT(1) FROM {0}".format(table))
     count = cur.fetchone()[0]
     cur.close()
     conn.close()
 
     return count
+
+
+def helper_get_db_conn(db_type, username):
+    return f.get_db_connection(db_type,
+                               username,
+                               test_parameters["password"],
+                               "localhost",
+                               f.get_default_db_port(db_type),
+                               test_parameters["database"])
 
 
 if __name__ == '__main__':
